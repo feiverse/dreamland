d3809d5fa63f77ac8ec16eb47f7c4ffb0a6d0fd8
Fei Wei <fwei@f4r12s9.clusters.42paris.fr>
2025-08-19 02:14:09 +0200

    add

--- dream3/goodnight.c
+++ dream3/goodnight.c
@@ -7,23 +7,23 @@
 # define BUFFER_SIZE 37
 #endif
 
-char	*findv(char *dmv)
+char	*findv(char *t)
 {
 	int fei = 0;
 
-	while (dmv[fei])
+	while (t[fei])
 		fei++;
-	char *lsv = (char *)malloc(fei + 1);
-	if (!lsv)
+	char *tab = (char *)malloc(fei + 1);
+	if (!tab)
 		return NULL;
 	for (int v = 0; v <= fei; v++)
-		lsv[v] = dmv[v];
-	return lsv;
+		tab[v] = t[v];
+	return tab;
 }
 
 char	*get_next_line(int fd)
 {
-	static char dmt[BUFFER_SIZE];
+	static char	dmt[BUFFER_SIZE];
 	static int	w = 0;
 	static int	g = 0;
 	char		fei[77777];
@@ -37,7 +37,7 @@ char	*get_next_line(int fd)
 		{
 			g = read(fd, dmt, BUFFER_SIZE);
 			w = 0;
-			if (g <= 0);
+			if (g <= 0)
 				break;
 		}
 		fei[v] = dmt[w++];
@@ -50,6 +50,6 @@ char	*get_next_line(int fd)
 	}
 	fei[v] = '\0';
 	if (v == 0)
-		return 0;
+		return NULL;
 	return (findv(fei));
 }
