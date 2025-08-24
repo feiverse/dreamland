cd9229a96eb616e84e886d7bb96c45d2c3ccbf92
Fei Wei <fwei@f4r14s8.clusters.42paris.fr>
2025-08-24 03:42:59 +0200

    add

---
+++ dream3/avicii.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int	avicii(char *tim)
+{
+	int	a = 0;
+	int v = 0;
+	while (tim[v])
+	{
+		if (tim[v] == '(')
+			a++;
+		if (tim[v] == ')')
+			a--;
+		if (a < 0)
+			return 0;
+		v++;
+	}
+	return (a == 0);
+}
+
+void	rip(char *tim, int berg, int bro, int sis, char *a, int v)
+{
+	if (bro < 0 || sis < 0)
+		return;
+	if (tim[berg] == '\0')
+	{
+		a[v] = '\0';
+		if (avicii(a))
+			puts(a);
+		return;
+	}
+	if (tim[berg] == '(' && bro > 0)
+	{
+ 	   a[v] = ' ';
+ 	   rip(tim, berg + 1, bro - 1, sis, a, v + 1);
+	}
+	else if (tim[berg] == ')' && sis > 0) 
+	{
+ 	   a[v] = ' ';
+	    rip(tim, berg + 1, bro, sis - 1, a, v + 1);
+	}
+	a[v] = tim[berg];
+	rip(tim, berg + 1, bro, sis, a, v + 1);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac != 2)
+		return 1;
+	char *tim = av[1];
+	int	peace = 0, heaven = 0, v = 0;
+	while (tim[v])
+	{
+		if (tim[v] == '(')
+			peace++;
+		else if (tim[v] == ')')
+		{
+			if (peace > 0)
+				peace--;
+			else
+				heaven++;
+		}
+		v++;
+	}
+	char *soul = malloc(sizeof(char) * (v + 1));
+	if (!soul)
+		return 1;
+	rip(tim, 0, peace, heaven, soul, 0);
+	free(soul);
+	return 0;
+}
