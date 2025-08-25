5da81e2411f1d0b72fbd2cfb1cb05ed12fcc56b8
Fei Wei <fwei@f4r13s8.clusters.42paris.fr>
2025-08-25 13:28:47 +0200

    add

--- dream3/primo.c
+++ dream3/primo.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-void	acid(int *psy, int good, int tab, char *trip)
+void	ams(int *psy, int room, int mush, char *good)
 {
-	int v;
+	int	v;
 
-	if (tab == good)
+	if (mush == room)
 	{
-		trip[good] = '\0';
-		puts(trip);
+		good[room] = '\0';
+		puts(good);
 		return;
 	}
 	for (v = 0; v < 256; ++v)
 		if (psy[v])
 		{
-			trip[tab] = (char)v;
+			good[mush] = (char)v;
 			--psy[v];
-			acid(psy, good, tab + 1, trip);
+			ams(psy, room, mush + 1, good);
 			++psy[v];
 		}
 }
 
-int main(int ac, char **av)
+int	main(int ac, char **av)
 {
 	if (ac == 2 && av[1][0])
 	{
 		int fei[256] = {0};
-		int tab = strlen(av[1]);
-		char *trip = malloc(tab + 1);
+		int age = strlen(av[1]);
+		char *god = malloc(age + 1);
 
-		for (int v = 0; v < tab; ++v)
+		for (int v = 0; v < age; ++v)
 			++fei[(unsigned char)av[1][v]];
-		acid(fei, tab, 0, trip);
-		free(trip);
+		ams(fei, age, 0, god);
+		free(god);
 	}
 	return 0;
 }
