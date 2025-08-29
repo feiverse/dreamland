5f7adb615c25161c6ac36ee2af3ce6044496b8ad
Fei Wei <fwei@f4r10s3.clusters.42paris.fr>
2025-08-29 02:04:59 +0200

    add

--- dream3/avicii.c
+++ dream3/avicii.c
@@ -1,71 +1,54 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int	avic(char *tim)
+int avic(char *tim)
 {
-	int a = 0;
-	int v = 0;
-	while (tim[v])
-	{
-		if (tim[v] == '(')
-			a++;
-		if (tim[v] == ')')
-			a--;
-		if (a < 0)
-			return 0;
-		v++;
-	}
-	return (a == 0);
+    int v = 0;
+    while (*tim)
+    {
+        if (*tim == '(') v++;
+        if (*tim == ')') v--;
+        if (v < 0) return 0;
+        tim++;
+    }
+    return v == 0;
 }
 
-void	rave(char *t, int w, int acid, int trap, char *beat, int v)
+void rave(char *a, int v, int acid, int trap)
 {
-	if (acid < 0 || trap < 0)
-		return;
-	if (t[w] == '\0')
+    if (!acid && !trap)
 	{
-		beat[v] = '\0';
-		if (avic(beat))
-			puts(beat);
-		return;
+		if (avic(a))
+			puts(a);
+			return;
 	}
-	if (t[w] == '(' && acid > 0)
-	{
- 		beat[v] = ' ';
- 		rave(t, w + 1, acid - 1, trap, beat, v + 1);
-	}
-	else if (t[w] == ')' && trap > 0) 
-	{
-		beat[v] = ' ';
-		rave(t, w + 1, acid, trap - 1, beat, v + 1);
-	}
-	beat[v] = t[w];
-	rave(t, w + 1, acid, trap, beat, v + 1);
+    for (; a[v]; v++)
+    {
+        if (a[v] == '(' && acid)
+        {
+            a[v] = ' ';
+            rave(a, v + 1, acid - 1, trap);
+            a[v] = '(';
+        }
+        if (a[v] == ')' && trap)
+        {
+            a[v] = ' ';
+            rave(a, v + 1, acid, trap - 1);
+            a[v] = ')';
+        }
+    }
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	if (ac != 2)
-		return 1;
-	char *soul = av[1];
-	int	trance = 0, techno = 0, v = 0;
-	while (soul[v])
-	{
-		if (soul[v] == '(')
-			trance++;
-		else if (soul[v] == ')')
-		{
-			if (trance > 0)
-				trance--;
-			else
-				techno++;
-		}
-		v++;
-	}
-	char *fee = malloc(sizeof(char) * (v + 1));
-	if (!fee)
-		return 1;
-	rave(soul, 0, trance, techno, fee, 0);
-	free(fee);
-	return 0;
+    if (ac == 2)
+    {
+        int l = 0, s = 0, tab = 0;
+        char *soul = av[1];
+        for (int v = 0; soul[v]; v++)
+            if (soul[v] == '(') tab++;
+            else if (soul[v] == ')') (tab ? tab-- : s++);
+        l = tab;
+        rave(soul, 0, l, s);
+    }
+    return 0;
 }
